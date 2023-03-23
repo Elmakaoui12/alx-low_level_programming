4d1a5e30f5ffaaf7bc7a6cc39e44cbc237e3163a
El makaoui <hichamelmakaoui@gmail.com>
2023-03-23 07:23:30 -0700

    jkj

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *positive_or_negative - checks for positive or negative numbers 
+ *
+ * @i: the number to be checked
+ * 
+ * return: always.
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d in negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
